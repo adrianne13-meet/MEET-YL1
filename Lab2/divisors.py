44575a0bc82e8ff63c3bd79d91a007b241f2beb9
Guest <guest-x7SPDH@meet.mit.edu>
2013-10-25 15:47:02 +0300

    Lab 2

--- Lab2/divisors.py
+++ Lab2/divisors.py
@@ -1,2 +1,10 @@
-n = raw_input("Insert a number")
-	print 
+
+def divisors(n):
+	n = int(n)
+	for x in xrange(n):
+   		if  n% (x + 1) == 0:
+        		print x + 1
+
+if __name__ == '__main__':
+	n = raw_input ("Insert a number ")
+	divisors(n)
